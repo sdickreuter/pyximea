44dc69dc543dd5763d381b399e2c733e16e7051c
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2016-04-21 12:55:12 +0200

    fixed imports and builds

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import numpy
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
 
 if platform.system() == 'Darwin':
     includes = [numpy.get_include()]
@@ -19,18 +21,23 @@ else:
 
 # extra_objects=["../build/libnanovg.a"]
 
-extensions = [
-    Extension(  "ximea", ['ximea/ximea.pyx'],
+extensions = cythonize([
+    Extension(  "ximea.ximea", ['ximea/ximea.pyx','ximea/cximea.pxd'],
                 include_dirs = includes,
                 libraries = libs,
                 extra_link_args=link_args,
                 extra_compile_args=['-std=c99']
-            ),
-]
+            )
+    ])
 
-setup(  name="ximea",
-        version="0.0.1",
-        description="Ximea XiAPI Python Bindings",
-        packages=['ximea'],
-        ext_modules=cythonize(extensions)
+setup(
+    name="ximea",
+    version="0.0.1",
+    description="Ximea XiAPI Python Bindings",
+    cmdclass={'build_ext': build_ext},
+    packages=['ximea'],
+    ext_modules=extensions
 )
+
+
+
